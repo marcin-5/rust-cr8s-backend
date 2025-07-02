b7c3fce7ed346eddc9765d57e4ddcee0a7ad17a5
Marcin Bojara <marcin_5@mail.ru>
2025-07-02 16:55:40 +0200

    Hash passwords securely with `argon2` in `create_user` function
    - Implement `argon2` password hashing using generated salt for `create_user`.
    - Update `commands.rs` to integrate secure password hashing logic.

--- src/commands.rs
+++ src/commands.rs
@@ -1,3 +1,6 @@
+use argon2::password_hash::rand_core::OsRng;
+use argon2::password_hash::SaltString;
+use argon2::PasswordHasher;
 use diesel_async::{AsyncConnection, AsyncPgConnection};
 
 use crate::{
@@ -14,6 +17,11 @@ async fn load_db_connection() -> AsyncPgConnection {
 
 pub async fn create_user(username: String, password: String, role_codes: Vec<String>) {
     let mut c = load_db_connection().await;
+
+    let salt = SaltString::generate(OsRng);
+    let argon2 = argon2::Argon2::default();
+    let password_hash = argon2.hash_password(password.as_bytes(), &salt).unwrap();
+    let password = password_hash.to_string();
 
     let new_user = NewUser { username, password };
     let user = UserRepository::create_with_roles(&mut c, new_user, role_codes)
