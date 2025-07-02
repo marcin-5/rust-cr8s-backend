83c094dd80d28af2169d42b5a439e43786df14ce
Marcin Bojara <marcin_5@mail.ru>
2025-07-02 22:07:12 +0200

    Refactor `create_user` to use `auth::hash_password` for improved password hashing consistency

--- src/commands.rs
+++ src/commands.rs
@@ -1,9 +1,8 @@
-use argon2::password_hash::rand_core::OsRng;
-use argon2::password_hash::SaltString;
 use argon2::PasswordHasher;
 use diesel_async::{AsyncConnection, AsyncPgConnection};
 
 use crate::{
+    auth,
     models::NewUser,
     repositories::{RoleRepository, UserRepository},
 };
@@ -18,12 +17,11 @@ async fn load_db_connection() -> AsyncPgConnection {
 pub async fn create_user(username: String, password: String, role_codes: Vec<String>) {
     let mut c = load_db_connection().await;
 
-    let salt = SaltString::generate(OsRng);
-    let argon2 = argon2::Argon2::default();
-    let password_hash = argon2.hash_password(password.as_bytes(), &salt).unwrap();
-    let password = password_hash.to_string();
-
-    let new_user = NewUser { username, password };
+    let password_hash = auth::hash_password(password).unwrap();
+    let new_user = NewUser {
+        username,
+        password: password_hash,
+    };
     let user = UserRepository::create_with_roles(&mut c, new_user, role_codes)
         .await
         .unwrap();
