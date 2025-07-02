966ef14a5342e66adae38be10da4ca80f6cc017f
Marcin Bojara <marcin_5@mail.ru>
2025-07-02 21:58:10 +0200

    Mount `login` route to server configuration

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -4,6 +4,10 @@ use rocket_db_pools::Database;
 #[rocket::main]
 async fn main() {
     let _ = rocket::build()
+        .mount(
+            "/",
+            rocket::routes![backend::rocket_routes::authorization::login],
+        )
         .mount("/rustaceans", backend::rocket_routes::rustaceans::routes())
         .mount("/crates", backend::rocket_routes::crates::routes())
         .attach(backend::rocket_routes::DbConn::init())
