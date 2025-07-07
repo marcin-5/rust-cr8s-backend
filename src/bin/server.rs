cf2ff47e43d50bbaf1b771caa3b0d5bbf1d08467
Marcin Bojara <marcin_5@mail.ru>
2025-07-07 20:24:21 +0200

    Integrate Redis session management into `login` endpoint
    - Attach `CacheConn` to Rocket server configuration.
    - Extend `login` functionality to store session IDs in Redis with expiration.

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -10,6 +10,7 @@ async fn main() {
         )
         .mount("/rustaceans", backend::rocket_routes::rustaceans::routes())
         .mount("/crates", backend::rocket_routes::crates::routes())
+        .attach(backend::rocket_routes::CacheConn::init())
         .attach(backend::rocket_routes::DbConn::init())
         .launch()
         .await;
--- src/rocket_routes/authorization.rs
+++ src/rocket_routes/authorization.rs
@@ -1,23 +1,31 @@
 use crate::auth::{authorize_user, Credentials};
 use crate::repositories::UserRepository;
-use crate::rocket_routes::server_error;
-use crate::rocket_routes::DbConn;
+use crate::rocket_routes::{server_error, CacheConn, DbConn};
+use rocket::http::Status;
 use rocket::response::status::Custom;
 use rocket::serde::json::{serde_json::json, Json, Value};
+use rocket_db_pools::deadpool_redis::redis::AsyncCommands;
 use rocket_db_pools::Connection;
 
 #[rocket::post("/login", format = "json", data = "<credentials>")]
 pub async fn login(
     mut db: Connection<DbConn>,
+    mut cache: Connection<CacheConn>,
     credentials: Json<Credentials>,
 ) -> Result<Value, Custom<Value>> {
-    UserRepository::find_by_username(&mut db, &credentials.username)
+    let user = UserRepository::find_by_username(&mut db, &credentials.username)
         .await
-        .map(|user| {
-            if let Ok(token) = authorize_user(&user, credentials.into_inner()) {
-                return json!(token);
-            }
-            json!("Unauthorized")
-        })
-        .map_err(|e| server_error(e.into()))
+        .map_err(|e| server_error(e.into()))?;
+
+    let session_id = authorize_user(&user, credentials.into_inner())
+        .map_err(|_| Custom(Status::Unauthorized, json!("Wrong credentials")))?;
+
+    cache
+        .set_ex::<String, i32, ()>(format!("sessions/{}", session_id), user.id, 3 * 60 * 60)
+        .await
+        .map_err(|e| server_error(e.into()))?;
+
+    Ok(json!({
+        "token": session_id,
+    }))
 }
