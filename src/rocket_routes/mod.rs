daf7983cf37fffeedce0b93723333748a0981945
Marcin Bojara <marcin_5@mail.ru>
2025-07-07 18:17:48 +0200

    Add `server_error` utility function for consistent JSON error responses
    - Implement `server_error` to handle internal server errors with JSON payload.
    - Update `rocket_routes` with necessary imports for error handling.

--- src/rocket_routes/mod.rs
+++ src/rocket_routes/mod.rs
@@ -1,3 +1,9 @@
+use rocket::http::Status;
+use rocket::response::status::Custom;
+use rocket::serde::json::serde_json::json;
+use rocket::serde::json::Value;
+use std::error::Error;
+
 pub mod authorization;
 pub mod crates;
 pub mod rustaceans;
@@ -5,3 +11,8 @@ pub mod rustaceans;
 #[derive(rocket_db_pools::Database)]
 #[database("postgres")]
 pub struct DbConn(rocket_db_pools::diesel::PgPool);
+
+pub fn server_error(e: Box<dyn Error>) -> Custom<Value> {
+    rocket::error!("{}", e);
+    Custom(Status::InternalServerError, json!("Error"))
+}
