8edbb47c04eca1a01041f4c29839f339359abdcc
Marcin Bojara <marcin_5@mail.ru>
2025-07-10 12:13:18 +0200

    Implement `FromRequest` for `User` to enable session-based user extraction
    - Add logic to extract and authenticate users from the `Authorization` header.
    - Integrate Redis (session cache) and Postgres (database) connections for session validation.

--- src/rocket_routes/mod.rs
+++ src/rocket_routes/mod.rs
@@ -1,7 +1,13 @@
+use crate::models::User;
+use crate::repositories::UserRepository;
 use rocket::http::Status;
+use rocket::request::{FromRequest, Outcome};
 use rocket::response::status::Custom;
 use rocket::serde::json::serde_json::json;
 use rocket::serde::json::Value;
+use rocket::Request;
+use rocket_db_pools::deadpool_redis::redis::AsyncCommands;
+use rocket_db_pools::Connection;
 use std::error::Error;
 
 pub mod authorization;
@@ -19,4 +25,39 @@ pub struct CacheConn(rocket_db_pools::deadpool_redis::Pool);
 pub fn server_error(e: Box<dyn Error>) -> Custom<Value> {
     rocket::error!("{}", e);
     Custom(Status::InternalServerError, json!("Error"))
+}
+
+#[rocket::async_trait]
+impl<'r> FromRequest<'r> for User {
+    type Error = ();
+    async fn from_request(req: &'r Request<'_>) -> Outcome<Self, Self::Error> {
+        // Authorization: Bearer SESSION_ID_128_CHARACTERS_LONG
+        let session_header = req
+            .headers()
+            .get_one("Authorization")
+            .map(|v| v.split_whitespace().collect::<Vec<_>>())
+            .filter(|v| v.len() == 2 && v[0] == "Bearer");
+
+        if let Some(header_value) = session_header {
+            let Outcome::Success(mut cache) = req.guard::<Connection<CacheConn>>().await else {
+                rocket::error!("Failed to get Redis connection from pool");
+                return Outcome::Error((Status::InternalServerError, ()));
+            };
+            let Outcome::Success(mut db) = req.guard::<Connection<DbConn>>().await else {
+                rocket::error!("Failed to get Postgres connection from pool");
+                return Outcome::Error((Status::InternalServerError, ()));
+            };
+
+            let key = format!("sessions/{}", header_value[1]);
+            let result: Result<i32, _> = cache.get(key).await;
+
+            if let Ok(user_id) = result {
+                if let Ok(user) = UserRepository::find(&mut db, user_id).await {
+                    return Outcome::Success(user);
+                }
+            }
+        }
+
+        Outcome::Error((Status::Unauthorized, ()))
+    }
 }
