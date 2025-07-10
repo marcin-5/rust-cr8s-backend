5b7542a5ac02ccecbb216b786e72ddc313c9960e
Marcin Bojara <marcin_5@mail.ru>
2025-07-10 21:33:22 +0200

    Add test to validate unauthorized access to private routes

--- tests/authorization.rs
+++ tests/authorization.rs
@@ -24,3 +24,46 @@ fn test_login() {
         .unwrap();
     assert_eq!(response.status(), StatusCode::UNAUTHORIZED);
 }
+
+#[test]
+fn test_unauthorized_access_to_private_routes() {
+    let client = Client::new();
+
+    // A list of private routes to test. We check different methods (GET, POST, PUT, DELETE).
+    let private_routes = vec![
+        ("GET", common::RUSTACEANS_URL.to_string()),
+        ("POST", common::RUSTACEANS_URL.to_string()),
+        ("GET", format!("{}/1", common::RUSTACEANS_URL)),
+        ("PUT", format!("{}/1", common::RUSTACEANS_URL)),
+        ("DELETE", format!("{}/1", common::RUSTACEANS_URL)),
+        ("GET", common::CRATES_URL.to_string()),
+        ("POST", common::CRATES_URL.to_string()),
+        ("GET", format!("{}/1", common::CRATES_URL)),
+        ("PUT", format!("{}/1", common::CRATES_URL)),
+        ("DELETE", format!("{}/1", common::CRATES_URL)),
+    ];
+
+    for (method, url) in private_routes {
+        let mut request = match method {
+            "GET" => client.get(&url),
+            "POST" => client.post(&url),
+            "PUT" => client.put(&url),
+            "DELETE" => client.delete(&url),
+            _ => panic!("Unsupported HTTP method in test"),
+        };
+
+        // Add a dummy body for methods that require it.
+        if method == "POST" || method == "PUT" {
+            request = request.json(&json!({}));
+        }
+
+        let response = request.send().unwrap();
+        assert_eq!(
+            response.status(),
+            StatusCode::UNAUTHORIZED,
+            "Expected UNAUTHORIZED for {} {}",
+            method,
+            url
+        );
+    }
+}
